f9d8b7e31cd827c7ee195850f18516da9caa26a2
crypticsoft <crypticsoft@gmail.com>
2013-09-01 17:46:18 -0700

    updated joyride plugin to v2.0.2, plugin.js to parse JSON into <ol> tag

--- joyride/plugin.js
+++ joyride/plugin.js
@@ -3,7 +3,10 @@ $(document).ready( function() {
 
 if(window.location.hash){  // Fragment exists
 	var hash = window.location.hash.substring(1);//Puts hash in variable, and removes the # character
+	var tours = JSON.parse($("#tour_data").html());
+/*
 	var tours = {
+
 		'tour-title' : { //#hashtag
 			'steps' : [
 				{
@@ -33,27 +36,29 @@ if(window.location.hash){  // Fragment exists
 
 			]//end 'steps'
 		} //end tour-video		
+
 	};//end tours
+*/
 
 	if(hash!=''){
 		
 		//set the container for the tip(s)
 		var tip_id = 'joyRideTipContent-' + hash;
-		var tips = '<ol id="' + tip_id + '">';
+		var tips = $("<ol/>",{ id: tip_id });
 		var dat_class, dat_id;
 
 		$.each(tours, function(i, val) {
 			if(i==hash){ //if we have a match, parse out the object values into list items
 				$.each(val.steps, function() {
-					
 					dat_class = this.class != undefined ? ' data-class="' + this.class + '"' : '';
 					dat_id = this.id != undefined ? ' data-id="' + this.id + '"' : '';
+					dat_options = this.options != undefined ? ' data-options="' + this.options + '"' : '';
 					// data-id  - data-class are optional attributres, if neither is found then modal window is used
-					tips += '<li data-text="' + this.text + '"' + dat_id + dat_class + '><h2>' + this.title + '</h2><p>' + this.content + '</p></li>';
+					tips.append('<li data-text="' + this.text + '"' + dat_id + dat_class + dat_options + '><h2>' + this.title + '</h2><p>' + this.content + '</p></li>');
 				});
 			}//end if
 		});//end each
-		tips += '</ol>';
+
 		$('body').append(tips); //append tip(s) to the body
 
 		if( $('#'+tip_id).length > 0 ){
@@ -83,7 +88,15 @@ if(window.location.hash){  // Fragment exists
 
 				}, 500);
 			});
+			setTimeout(function() {		
+				$('#'+tip_id).joyride();	
+				// animate top position to consider the wpadminbar height, need to find solution for the first slide
+				if( $('#wpadminbar').length > 0 ){
+					var ntop = $('#wpadminbar').height();					
+					$('.joyride-tip-guide:visible').animate({top: '-=' + ntop });
+				}
 
+			}, 1000);
 
 		} 
 
